66f23a320ac97e468817d479a2c18a1b0b659341
Timothy Mastny <tim.mastny@gmail.com>
2019-02-23 08:30:45 -0600

    latest work

---
+++ e10026.cpp
@@ -0,0 +1,49 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <map>
+#include <deque>
+#include <iterator>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> job_sequence(vector<tuple<int, int, int>> &tasks) {
+  vector<int> v = {1, 2, 3};
+  return v;
+}
+
+void print_sequence(vector<int> &seq) {
+  for (auto el : seq) {
+    cout << el << " ";
+  }
+  cout << endl;
+}
+
+int main() {
+
+  int cases;
+  cin >> cases;
+
+  for (int i = 0; i < cases; i++) {
+    string empty_line;
+    getline(cin, empty_line);
+
+
+    vector<tuple<int, int, int>> tasks;
+    int tasks_number;
+    cin >> tasks_number;
+    for (int j = 0; j < tasks_number; j++) {
+      int time;
+      int fine;
+      cin >> time;
+      cin >> fine;
+
+      tasks.push_back(make_tuple(j + 1, time, fine));
+    }
+
+    vector<int> seq = job_sequence(tasks);
+    print_sequence(seq);
+  }
+}
--- e10041.cpp
+++ e10041.cpp
@@ -9,32 +9,39 @@
 
 using namespace std;
 
+int minimum_distance_to_relatives(vector<int> &addresses) {
+  int n = (addresses.size() - 1) / 2;
+
+  nth_element(addresses.begin(), addresses.begin() + n, addresses.end());
+
+  int median = addresses[n];
+  int distance = 0;
+  for (int i = 0; i < addresses.size(); i++) {
+    distance += abs(addresses[i] - median);
+  }
+
+  return distance;
+}
+
 int main() {
 
   int cases;
   cin >> cases;
 
   for (int i = 0; i < cases; i++) {
+    int relatives;
+    cin >> relatives;
+
+    vector<int> addresses;
     string line;
-    while (getline(cin, line)) {
+    getline(cin, line);
+    std::istringstream iss(line);
 
+    int n;
+    while(iss >> n) {
+      addresses.push_back(n);
     }
-  }
 
-  string a;
-  string b;
-  while(getline(cin, a)) {
-    getline(cin, b);
-
-    sort(a.begin(), a.end());
-    sort(b.begin(), b.end());
-
-    deque<char> qa;
-    qa.assign(a.begin(), a.end());
-
-    deque<char> qb;
-    qb.assign(b.begin(), b.end());
-
-    cout << matching_letters(qa, qb) << endl;
+    cout << minimum_distance_to_relatives(addresses) << endl;
   }
 }
