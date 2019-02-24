0ed401972fed045c64cc7496e58f7e5d20700c35
Timothy Mastny <timothy.mastny@hudl.com>
2019-02-23 21:15:46 -0600

    trying struct

--- e10026.cpp
+++ e10026.cpp
@@ -9,15 +9,35 @@
 
 using namespace std;
 
-vector<int> job_sequence(vector<tuple<int, int, int>> &tasks) {
-
-  vector<int> v = {1, 2, 3};
-  return v;
+struct job {
+  int num;
+  int time;
+  int fine;
 }
 
-void print_sequence(vector<int> &seq) {
+bool sort_by_third(const tuple<int, int, int> &a, const tuple<int, int, int> &b) {
+  return get<2>(a) > get<2>(b);
+}
+
+bool max_job()
+
+void sort_jobs(vector<tuple<int, int, int>> &tasks) {
+  int days_remaining = tasks.size();
+
+  vector<tuple<int, int>> delayed_costs;
+  for (auto job : tasks) {
+    int job_cost = (days_remaining - get<1>(job)) * get<2>(job);
+    delayed_costs.push_back(make_tuple(get<0>(job), job_cost);
+  }
+
+  auto next_job = max_element(delayed_costs.begin(), delayed_costs.end());
+
+  sort(tasks.begin(), tasks.end(), sort_by_third);
+}
+
+void print_job_order(vector<tuple<int, int, int>> &seq) {
   for (auto el : seq) {
-    cout << el << " ";
+    cout << get<0>(el) << " ";
   }
   cout << endl;
 }
@@ -38,13 +58,12 @@ int main() {
     for (int j = 0; j < tasks_number; j++) {
       int time;
       int fine;
-      cin >> time;
-      cin >> fine;
+      cin >> time >> fine;
 
       tasks.push_back(make_tuple(j + 1, time, fine));
     }
 
-    vector<int> seq = job_sequence(tasks);
-    print_sequence(seq);
+    sort_jobs(tasks);
+    print_job_order(tasks);
   }
 }
