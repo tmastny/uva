8b07336404febb8bae2f24ae54b442ed7e49a34f
Timothy Mastny <tim.mastny@gmail.com>
2019-02-26 07:33:33 -0600

    save

--- e10026.cpp
+++ e10026.cpp
@@ -6,6 +6,8 @@
 #include <deque>
 #include <iterator>
 #include <algorithm>
+#include <utility>
+#include <cmath>
 
 #define INDEX 0
 #define TIME 1
@@ -13,8 +15,8 @@
 
 using namespace std;
 
-bool compare2(const tuple<int, int> &a, const tuple<int, int> &b) {
-  return get<1>(a) < get<1>(b);
+bool compare(tuple<int, int ,int> &a, tuple<int, int ,int> &b) {
+  return get<INDEX>(a) < get<INDEX>(b);
 }
 
 int task_time(vector<tuple<int, int, int>> &tasks) {
@@ -26,16 +28,14 @@ int task_time(vector<tuple<int, int, int>> &tasks) {
   return sum;
 }
 
-void job_order(vector<tuple<int, int, int>> &tasks, vector<int> &order) {
-  //vector<int> order;
-  order.clear();
+void job_order(vector<tuple<int, int, int>> &tasks) {
   int days_left = task_time(tasks);
 
-  while (tasks.size() > 0) {
+  for (int i = 0; i < tasks.size(); i++) {
 
     int max_delay_cost = 0;
-    auto max_it = tasks.begin();
-    for (auto it = tasks.begin(); it != tasks.end(); it++) {
+    auto max_it = tasks.begin() + i;
+    for (auto it = max_it; it != tasks.end(); it++) {
 
       int delay_cost = (days_left - get<TIME>(*it)) * get<FINE>(*it);
 
@@ -45,18 +45,14 @@ void job_order(vector<tuple<int, int, int>> &tasks, vector<int> &order) {
       }
     }
 
-    order.push_back(get<INDEX>(*max_it));
+    swap(*max_it, tasks[i]);
     days_left -= get<TIME>(*max_it);
-
-    tasks.erase(max_it);
   }
-
-  //return order;
 }
 
-void print_job_order(vector<int> &seq) {
+void print_job_order(vector<tuple<int, int, int>> &seq) {
   for (int i = 0; i < seq.size(); i++) {
-    cout << seq[i];
+    cout << get<INDEX>(seq[i]);
     if (i < seq.size() - 1) {
       cout << " ";
     }
@@ -74,6 +70,22 @@ int job_cost(vector<tuple<int, int, int>> &tasks) {
   }
 
   return cost;
+}
+
+int min_job_cost(vector<tuple<int, int, int>> &tasks) {
+  sort(tasks.begin(), tasks.end(), compare);
+
+  int min_cost = job_cost(tasks);
+  for (int i = 0; i < tgamma(tasks.size() + 1); i++) {
+    int cost = job_cost(tasks);
+    if (cost < min_cost) {
+      min_cost = cost;
+      print_job_order(tasks);
+    }
+    next_permutation(tasks.begin(), tasks.end(), compare);
+  }
+
+  return min_cost;
 }
 
 int main() {
@@ -99,10 +111,16 @@ int main() {
 
       tasks.push_back(make_tuple(j + 1, time, fine));
     }
+    cout << endl;
+    cout << "Task: " << i << " " << tasks_number << endl;
 
-    cout << job_cost(tasks) << endl;
+    int best_min = min_job_cost(tasks);
+    cout << "Min cost: " << best_min << endl;
 
-    job_order(tasks, jobs);
-    print_job_order(jobs);
+    job_order(tasks);
+
+    cout << "Found cost: " << job_cost(tasks) << endl;
+
+    print_job_order(tasks);
   }
 }
